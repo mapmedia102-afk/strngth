0df1308985c3f5766910150c80b589530fd47ae7
vaghani het <vaghanihet@vaghanis-MacBook-Air.local>
2026-05-23 11:12:04 +0530

    Connect to Shopify with storefront token

---
+++ src/lib/shopify.ts
@@ -0,0 +1,47 @@
+import { GraphQLClient } from "graphql-request";
+
+const domain = process.env.NEXT_PUBLIC_SHOPIFY_DOMAIN || "glocasaglasswear.myshopify.com";
+const storefrontToken = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_TOKEN || "YOUR_STOREFRONT_TOKEN";
+
+export const shopify = new GraphQLClient(
+  `https://${domain}/api/2024-04/graphql.json`,
+  {
+    headers: {
+      "X-Shopify-Storefront-Access-Token": storefrontToken,
+    },
+  }
+);
+
+// Queries
+export const GET_PRODUCTS = `
+query GetProducts($first: Int!) {
+  products(first: $first) {
+    edges {
+      node {
+        id
+        title
+        description
+        priceRange {
+          minVariantPrice {
+            amount
+            currencyCode
+          }
+        }
+        images(first: 1) {
+          edges { node { src altText } }
+        }
+      }
+    }
+  }
+}`;
+
+export const GET_PRODUCT = `
+query GetProduct($id: ID!) {
+  product(id: $id) {
+    id
+    title
+    description
+    priceRange { minVariantPrice { amount currencyCode } }
+    images(first: 5) { edges { node { src altText } } }
+  }
+}`;
